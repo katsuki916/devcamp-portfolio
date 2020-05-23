9ad52913a4279c9c2968290472b73d8f99913ec8
katsu916 <thedaisy916+dev@gmail.com>
2020-05-23 15:52:35 +0900

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,9 @@ puts "5 skills creates"
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "My Greate Service",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nihilsi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.jp/600x400.png",
+    thumb_image: "http://placehold.jp/350x200.png"
   )
 end
 
